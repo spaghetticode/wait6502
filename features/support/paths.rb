19abebe035f4fb597b733a98f0ff2343ec48e9fb
Andrea Longhi <andrea74@mailshack.com>
2009-10-19 23:01:34 +0200

    added basic storage devices feature

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -12,9 +12,9 @@ module NavigationHelpers
       '/'
     when /the user login page/
       new_user_session_path
-    when /the new (currency|country|computer type|manufacturer|user|io port) page/
+    when /the new (currency|country|computer type|manufacturer|user|io port|storage device) page/
       send("new_admin_#{$1.gsub(' ', '_')}_path")
-    when /the (currencies|countries|computer types|manufacturers|io ports) page/
+    when /the (currencies|countries|computer types|manufacturers|io ports|storage devices) page/
       send ("admin_#{$1.gsub(' ', '_')}_path")
     # Add more mappings here.
     # Here is a more fancy example:
