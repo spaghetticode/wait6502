285a64d6bfccd6e6aa98efe529b5289c4d0efc96
Andrea Longhi <andrea74@mailshack.com>
2009-11-04 23:37:03 +0100

    some image model refactoring

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,12 +1,12 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
   
-  attr_accessor :uploaded_file, :original_file
+  attr_accessor :uploaded_file
   
   validates_presence_of :imageable_id, :imageable_type
-  validate_on_create :validate_file_format
+  validate_on_create    :validate_file_format
   
-  before_create :save_files
+  before_create  :save_files
   before_destroy :delete_files
   
   FORMATS = { :thumb => 'x50', :small => 'x150', :normal => 'x400' }
@@ -15,7 +15,7 @@ class Image < ActiveRecord::Base
   FS_PREFIX = File.join(RAILS_ROOT, 'public', DB_PREFIX)
   
   def filename(format=:normal, prefix=:db)
-    "#{prefix == :db ? DB_PREFIX : FS_PREFIX}/#{format.to_s}/#{original_filename}"
+    "#{prefix == :db ? DB_PREFIX : FS_PREFIX}/#{format}/#{original_filename}"
   end
   
   def title
@@ -32,30 +32,34 @@ class Image < ActiveRecord::Base
   end
   
   def save_files
-    set_original_filename
-    save_original_file
-    FORMATS.each do |format, size|
-      system "convert -resize #{size} '#{original_file}' '#{filename(format, :fs)}'"
-    end
+    set_filenames
+    save_original_image
+    save_resized_images
   end
   
   def delete_files
     FORMATS.keys.each do |format|
-      File.delete(filename(format, :fs))
+      file = filename(format, :fs)
+      File.delete(file) if File.file?(file)
     end      
   end
   
-  def set_original_filename
+  def set_filenames
     self.original_filename = "#{Time.now.to_s(:number)}_#{uploaded_file.original_filename}"
+    @original_file = File.join(FS_PREFIX, 'original', original_filename)
   end
   
-  def save_original_file
-    self.original_file = File.join(FS_PREFIX, 'original', original_filename)
+  def save_original_image
     File.open(@original_file, 'wb') do |f|
-      while !uploaded_file.eof
+      until uploaded_file.eof
         f.write uploaded_file.read(4096)
       end
     end
   end
   
+  def save_resized_images
+    FORMATS.each do |format, size|
+      system "convert -resize #{size} '#{@original_file}' '#{filename(format, :fs)}'"
+    end
+  end
 end
