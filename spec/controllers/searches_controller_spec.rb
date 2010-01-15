ae6d060f6ca8f0bc4d7e44cd80cccfd8ca3a3c03
Andrea Longhi <andrea74@mailshack.com>
2010-01-15 16:15:55 +0100

    fixed builtin_language#to_param

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,11 +1,6 @@
 require 'spec_helper'
 
 describe SearchesController do
-
-  #Delete this example and add some real ones
-  it "should use SearchesController" do
-    controller.should be_an_instance_of(SearchesController)
-  end
   
   it 'should not raise a RedirectBackError' do
     lambda do
